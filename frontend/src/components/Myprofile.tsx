f3850b6d44cf1f6cdc64bfdb3e1546f657a32e1c
imrie123 <nosirie@ymobile.ne.jp>
2024-01-15 15:18:29 +0900

    Chore:Completed UI enhancements

--- frontend/src/components/Myprofile.tsx
+++ frontend/src/components/Myprofile.tsx
@@ -1,13 +1,15 @@
-import React, { useEffect } from 'react';
+import React, {useEffect} from 'react';
 import axios from 'axios';
-import { useState } from 'react';
+import {useState} from 'react';
 import SettingsIcon from '@mui/icons-material/Settings';
 import {Link} from "react-router-dom";
+import styles from "../styles/Myprofile.module.scss";
+import {Button} from "@chakra-ui/react";
+
 
 
 function MyProfile() {
-    const [user, setUser] = useState({ name: 'Loading...', learning_language: 'Loading...', spoken_language: 'Loading...' , residence: 'Loading...', introduction: 'Loading...'});
-
+    const [user, setUser] = useState({name: 'Loading...', learning_language: 'Loading...', spoken_language: 'Loading...', residence: 'Loading...', introduction: 'Loading...'});
 
 
     useEffect(() => {
@@ -26,21 +28,50 @@ function MyProfile() {
     }, []);
 
     return (
-        <div>
-            <Link to="/setting" >
-                <SettingsIcon />
-            </Link>
+        <>
+        <div className={styles.myprofile}>
+            <div className={styles.component}>
+                <div className={styles.top}>
 
-            <p>{user.name}</p>
-            <p>話せる言語:{user.spoken_language}</p>
-            <p>学びたい言語:{user.learning_language}</p>
-            <p>住んでいる国:{user.residence}</p>
-            <p>自己紹介:{user.introduction}</p>
+                    <div className={styles.introduce}>
 
 
+                        <img src="https://source.unsplash.com/random" alt="プロフィール画像"/>
+
+                        <div className={styles.info}>
+                            <div className={styles.follow}>
+                            <p>フォロー:100</p>
+                            <p>フォロワー:100</p>
+                            <p>投稿:20</p>
+                                <Link to="/setting">
+                                    <SettingsIcon/>
+                                </Link>
+                            </div>
+                        <div className={styles.user}>
+                        <p>{user.name}</p>
+                        <p>話せる言語:{user.spoken_language}</p>
+                        <p>学びたい言語:{user.learning_language}</p>
+                        <p>住んでいる国:{user.residence}</p>
+                        <p>自己紹介:{user.introduction}</p>
+
+                        </div>
+                            <Button>
+                                フォロー
+                            </Button>
+                        </div>
 
 
-        </div>
+
+                    </div>
+
+                    </div>
+                </div>
+            </div>
+
+
+
+</>
+
     );
 }
 
