b93beb5b86905f65d487c568af96e44ce497015e
imrie123 <nosirie@ymobile.ne.jp>
2024-05-12 14:30:46 +0900

    feat:introduce rspec

--- api/db/schema.rb
+++ api/db/schema.rb
@@ -115,6 +115,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_07_054622) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "name"
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_rooms_on_user_id"
   end
 
   create_table "user_rooms", charset: "utf8mb3", force: :cascade do |t|
